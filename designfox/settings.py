45225f4558e99b9f6ca82b45f9a3bf7466b20498
Laura Fox <laurafox91@gmail.com>
2024-12-12 14:38:28 +0000

    Updated shopping bag template when items added

--- designfox/settings.py
+++ designfox/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ibemji*9d94n%p8773-7@b3agt6pkmk5kp=gi9et(&h5+67k17'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-lauramayfox-designfox-3e54pjnt7b0.ws.codeinstitute-ide.net',
 '.herokuapp.com',]
