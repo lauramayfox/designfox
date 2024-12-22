3926e26055c4312a93d961e7a23c36343f37552b
Laura Fox <laurafox91@gmail.com>
2024-12-22 11:32:32 +0000

    updated checkout form

--- designfox/settings.py
+++ designfox/settings.py
@@ -49,9 +49,12 @@ INSTALLED_APPS = [
     'products',
     'bag',
     'checkout',
+    
 
     # Other
     'crispy_forms',
+    'crispy_bootstrap4',
+    
 ]
 
 MIDDLEWARE = [
@@ -184,6 +187,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 # Stripe
