8fc893ad081e364b6c927562b1aa01aeab2fb0b7
Laura Fox <laurafox91@gmail.com>
2024-12-24 17:28:37 +0000

    bebugging for AWS

--- designfox/settings.py
+++ designfox/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-lauramayfox-designfox-3e54pjnt7b0.ws.codeinstitute-ide.net',
 '.herokuapp.com',]
