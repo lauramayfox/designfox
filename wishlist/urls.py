580b23e40c1101e60e3f7ef8ebac8de97d1ee00b
Laura Fox <laurafox91@gmail.com>
2024-12-25 03:01:51 +0000

    wishlist logic updated, remove feature added

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -5,5 +5,7 @@ app_name = 'wishlist'  # Register the namespace for the wishlist app
 
 urlpatterns = [
     path('add/<int:product_id>/', views.add_to_wishlist, name='add_to_wishlist'),
-    path('', views.wishlist, name='wishlist'),  # Add a route for the wishlist view
+    path('', views.wishlist, name='wishlist'),
+    path('remove/<int:product_id>/', views.remove_from_wishlist, name='remove_from_wishlist'),
+
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -3,9 +3,11 @@ from .models import Wishlist
 from products.models import Product 
 
 def wishlist(request):
-    # Logic to display the wishlist
-    return render(request, 'wishlist/wishlist.html')
-
+    if request.user.is_authenticated:
+        wishlist_items = Wishlist.objects.filter(user=request.user)
+        return render(request, 'wishlist/wishlist.html', {'wishlist_items': wishlist_items})
+    else:
+        return redirect('login')
 
 
 def add_to_wishlist(request, product_id):
@@ -18,3 +20,13 @@ def add_to_wishlist(request, product_id):
         return redirect('wishlist:wishlist')  # Add the namespace
     else:
         return redirect('login')
+
+
+def remove_from_wishlist(request, product_id):
+    if request.user.is_authenticated:
+        product = get_object_or_404(Product, id=product_id)
+        Wishlist.objects.filter(user=request.user, product=product).delete()
+        return redirect('wishlist:wishlist')
+    else:
+        return redirect('login')
+
