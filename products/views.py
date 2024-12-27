e9db8f447d9b65e69c609a55c4581b6c4d5dec37
Laura Fox <laurafox91@gmail.com>
2024-12-27 16:42:12 +0000

    media queries wishlist and index and css updates

--- products/views.py
+++ products/views.py
@@ -75,7 +75,7 @@ def product_detail(request, product_id):
 def add_product(request):
     """ Add a product to the store """
     if not request.user.is_superuser:
-        messages.error(request, 'Sorry, only store owners can do that.')
+        messages.error(request, 'Sorry, only store owner can do that.')
         return redirect(reverse('home'))
 
     if request.method == 'POST':
