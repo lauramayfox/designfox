51b103067761d7e346bb1a4291cd102485b03354
Laura Fox <laurafox91@gmail.com>
2024-12-27 02:24:44 +0000

    updates to the migrations for deployment

--- designfox/settings.py
+++ designfox/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-lauramayfox-designfox-3e54pjnt7b0.ws.codeinstitute-ide.net',
 '.herokuapp.com', 'design-fox-0911e03a4419.herokuapp.com', 'localhost',]
